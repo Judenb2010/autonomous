02c88900edc1a870188bbac54e74982856c23aa3
Judenb2010 <judenb2010@gmail.com>
2022-02-18 10:48:57 -0500

    commit 1

---
+++ src/app/configuration.ts
@@ -0,0 +1,50 @@
+export const configuration = {
+  header: {
+    heading: 'Autonomous God',
+    headingtext: 'Namari is a free landing page template you can use for your projects.It is free to use for personal and commercial projects, enjoy!',
+    buttontext: 'do some action!',
+    buttonlink: '\home'
+  },
+  intro: {
+    tagline: 'SUCESS',
+    title: 'How We Help You To Sell Your Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \n eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+    features: [
+      { icon: 'html5', title: 'HTML5 &amp; CSS3', description: 'Has ne tritani atomorum conclusionemque, in dolorum volumus cotidieque eum. At vis choro neglegentur iudico' },
+      { icon: 'bolt', title: 'Easy to Use', description: 'Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sit detracto mediocrem disputationi' },
+      { icon: 'tablet', title: 'Fully Responsive', description: 'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro nemore vivendum' },
+      { icon: 'rocket', title: 'Parallax Effect', description: 'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro nemore vivendum' },
+
+    ]
+
+  },
+
+  services: {
+    tagline: 'BELIEVING',
+    title: 'Focusing On What Matters Most',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \n eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!'
+
+  },
+
+  testimonials: {
+    tagline: 'FEEDBACK',
+    title: 'What our customers are saying',
+    description: ''
+
+  },
+
+  clients: {
+    tagline: 'TRUST',
+    title: 'Companies who use our services',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \n eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!'
+
+  },
+
+  pricing: {
+    tagline: 'YOUR CHOICE',
+    title: 'We have the right package for you',
+    description: ''
+
+  },
+
+};
